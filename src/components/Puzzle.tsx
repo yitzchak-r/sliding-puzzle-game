314655e11768a0ed15e10c765df520edc88493f2
Yitzchak Rosenhan <yr058325101@gmail.com>
2024-10-29 11:38:01 +0200

    Define Puzzle component and initialize tile state with shuffle

--- src/components/Puzzle.tsx
+++ src/components/Puzzle.tsx
@@ -2,3 +2,17 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const GRID_SIZE = 3;
+
+const Puzzle: React.FC = () => {
+ 
+    const [tiles, setTiles] = useState<number[]>([]);
+  
+    useEffect(() => {
+      resetPuzzle();
+    }, []);
+  
+    const resetPuzzle = () => {
+      const shuffledTiles = shuffleTiles([...Array(GRID_SIZE * GRID_SIZE).keys()]); 
+      setTiles(shuffledTiles);
+    };
+  

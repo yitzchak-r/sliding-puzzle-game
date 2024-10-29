6bc9c3c314c14d3e74a5cc0f3577a61ff34f4052
Yitzchak Rosenhan <yr058325101@gmail.com>
2024-10-29 11:43:52 +0200

    Add shuffleTiles function to randomize tiles

--- src/components/Puzzle.tsx
+++ src/components/Puzzle.tsx
@@ -15,4 +15,12 @@ const Puzzle: React.FC = () => {
       const shuffledTiles = shuffleTiles([...Array(GRID_SIZE * GRID_SIZE).keys()]); 
       setTiles(shuffledTiles);
     };
-  
+    
+    const shuffleTiles = (tiles: number[]): number[] => {
+        for (let i = tiles.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [tiles[i], tiles[j]] = [tiles[j], tiles[i]];
+        }
+        return tiles;
+      };
+    
